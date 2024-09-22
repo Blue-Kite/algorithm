7e5bd2afcbcb7b549c31eb8cbc0a1b6bc68587c6
Blue-Kite <bluelemon112@gmail.com>
2024-09-22 22:16:30 +0900

    --allow-empty-message

---
+++ 코테100제/기능개발.py
@@ -0,0 +1,17 @@
+import math
+def solution(progresses, speeds):
+    answer = []
+    time = [math.ceil((100 - progresses[i])/speeds[i])for i in range(len(progresses))]
+    cnt = 0
+    std = time[0] #배포 기준점, 이거보다 작은 거는 묶어서 배포함 
+    
+    for t in time:
+        if t <= std:
+            cnt += 1
+        else:
+            answer.append(cnt)
+            cnt = 1
+            std = t      
+    
+    answer.append(cnt)
+    return answer
